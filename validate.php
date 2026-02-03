9ccebc05a129e3c5043a5c4e429a7fca4821afa0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-02-02 20:37:53 -0600

    Add number of invalid inputs

--- validate.php
+++ validate.php
@@ -9,80 +9,96 @@
     <h1>Validation Result</h1>
     <?php
     $name = $email = $date = $dateInput = $time = $timeInput = $datetimeInput = $datetimeLocalInput = $month = $week = $number = $range = $color = "";
+    $invalidInputs = 0;
+    $invalidInputFormats = 0;
     $nameErr = $emailErr = $dateErr = $dateInputErr = $timeErr = $timeInputErr = $datetimeInputErr = $datetimeLocalInputErr = $monthErr = $weekErr = $numberErr = $rangeErr = $colorErr = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty($_POST["name"])) {
             $nameErr = "Name is required";
+            $invalidInputs++;
         } else {
             $name = htmlspecialchars($_POST["name"]);
         }
 
         if (empty($_POST["email"])) {
             $emailErr = "Email is required";
+            $invalidInputs++;
         } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Invalid email format";
+            $invalidInputFormats;
         } else {
             $email = htmlspecialchars($_POST["email"]);
         }
 
         if (empty($_POST["date"])) {
             $dateErr = "Date is required";
+            $invalidInputs++;
         } else {
             $date = htmlspecialchars($_POST["date"]);
             $dateTimestamp = strtotime($dateInput);
             if ($dateTimestamp === false) {
                 $dateInputErr = "Invalid date format";
+                $invalidInputFormats;
             }
         }
 
         if (empty($_POST["time"])) {
             $timeErr = "Time is required";
+            $invalidInputs++;
         } else {
             $time = htmlspecialchars($_POST["time"]);
             $timeTimestamp = strtotime($timeInput);
             if ($timeTimestamp === false) {
                 $timeInputErr = "Invalid time format";
+                $invalidInputFormats;
             }
         }
 
         if (empty($_POST["datetimeInput"])) {
             $datetimeInputErr = "Datetime Input is required";
+            $invalidInputs++;
         } else {
             $datetimeInput = htmlspecialchars($_POST["datetimeInput"]);
         }
 
         if (empty($_POST["datetimeLocalInput"])) {
             $datetimeLocalInputErr = "Datetime Local Input is required";
+            $invalidInputs++;
         } else {
             $datetimeLocalInput = htmlspecialchars($_POST["datetimeLocalInput"]);
         }
 
         if (empty($_POST["month"])) {
             $monthErr = "Month is required";
+            $invalidInputs++;
         } else {
             $month = htmlspecialchars($_POST["month"]);
         }
 
         if (empty($_POST["week"])) {
             $weekErr = "Week is required";
+            $invalidInputs++;
         } else {
             $week = htmlspecialchars($_POST["week"]);
         }
 
         if (empty($_POST["number"])) {
             $numberErr = "Number is required";
+            $invalidInputs++;
         } else {
             $number = htmlspecialchars($_POST["number"]);
         }
 
         if (empty($_POST["range"])) {
             $rangeErr = "Range is required";
+            $invalidInputs++;
         } else {
             $range = htmlspecialchars($_POST["range"]);
         }
 
         if (empty($_POST["color"])) {
             $colorErr = "Color is required";
+            $invalidInputs++;
         } else {
             $color = htmlspecialchars($_POST["color"]);
         }
